586af33f878141ea14d25b7c45562d1d1e967e74
Chris Ivey <chrisivey01@gmail.com>
2018-06-17 14:29:17 -0500

    fixed ip

--- src/getMood.js
+++ src/getMood.js
@@ -1,5 +1,5 @@
-// const API = 'http://54.147.40.233:3000/'
-const API = 'http://localhost:3000/'
+const API = 'http://54.147.40.233:3000/'
+// const API = 'http://localhost:3000/'
 
 export default{
 
